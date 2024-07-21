b9393ede7c233211821e74453ccd423fc64351b7
mdhvkothari <mdhvkothari007@gmail.com>
2024-07-21 20:34:23 +0530

    leetcode - 103

---
+++ 103-Binary-Tree-Zigzag-Level-Order-Traversal.py
@@ -0,0 +1,22 @@
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        flag, result, q = 0, [], [root]
+        while q:
+            length = len(q)
+            temp = []
+            for i in range(length):
+                node = q.pop(0)
+                temp.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            if flag == 1:
+                temp = temp[::-1]
+                flag = 0
+            else:
+                flag = 1
+            result.append(temp)
+        return result
