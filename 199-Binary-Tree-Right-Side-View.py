c089e1bc5734f687fd6c6631cc085cf77027a36e
mdhvkothari <mdhvkothari007@gmail.com>
2024-07-23 20:12:42 +0530

    leetcode - 199

---
+++ 199-Binary-Tree-Right-Side-View.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+
+    def fun(self, root, level: int, ds: [int]) -> None:
+        if not root:
+            return
+        if len(ds) == level:
+            ds.append(root.val)
+        self.fun(root.right, level+1, ds)
+        self.fun(root.left, level+1, ds)
+    
+    def rightSideView(self, root) -> List[int]:
+        result = []
+        if not root:
+            return result
+        self.fun(root, 0, result)
+        return result
+
+
+############## 
+'''
+    For left view just put root.left above root.right
+'''
